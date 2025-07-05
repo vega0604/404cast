42dcbfdc3996565ef3eb6dfab09f07a54b2c8618
Aleksandar Bursac <aleksbrsc@gmail.com>
2025-07-05 19:18:52 -0400

    feat(frontend): UI for Sidebar.jsx actions and footer with like button

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -5,8 +5,24 @@ import sidebar from '@assets/images/icons/sidebar.svg';
 import play from '@assets/images/icons/play.svg';
 import leaderboard from '@assets/images/icons/leaderboard.svg';
 import heart from '@assets/images/icons/heart.svg';
+import { useState } from 'react';
+import * as Tooltip from '@radix-ui/react-tooltip';
 
 const Sidebar = () => {
+  const [likeCount, setLikeCount] = useState(0);
+  const [showTooltip, setShowTooltip] = useState(false);
+  const [tooltipTimeout, setTooltipTimeout] = useState(null);
+
+  const handleLikeClick = () => {
+    setLikeCount(prevCount => prevCount + 1);
+    setShowTooltip(true);
+    
+    if (tooltipTimeout) clearTimeout(tooltipTimeout);
+    
+    const newTimeout = setTimeout(() => setShowTooltip(false), 750);
+    setTooltipTimeout(newTimeout);
+  };
+  
   return (
     <nav id={styles.sidebar}>
         <div id={styles.header}>
@@ -18,13 +34,14 @@ const Sidebar = () => {
             <img id={styles.sidebar_toggle} src={sidebar} alt="sidebar" />
         </div>
 
-        {/* <div id={styles.actions}>
+        <div id={styles.actions}>
             <div className={styles.action}>
                 <img src={play} alt="Play icon"/>
-                
+                <h2>New Game</h2>
             </div>
             <div className={styles.action}>
-                <img src={play} alt="Play icon"/>
+                <img src={leaderboard} alt="Leaderboard icon"/>
+                <h2>Leaderboard</h2>
             </div>
         </div>
 
@@ -33,8 +50,30 @@ const Sidebar = () => {
         </div>
 
         <footer>
-
-        </footer> */}
+            <p id={styles.credits}>
+                <span>Made with</span> 
+                <span id={styles.emoji}>🕵️</span>
+            </p>
+            <div id={styles.like_container}>
+              <div>{likeCount}</div>
+              <Tooltip.Provider>
+                <Tooltip.Root open={showTooltip}>
+                  <Tooltip.Trigger asChild>
+                    <img src={heart} alt="Like button" onClick={handleLikeClick}/>
+                  </Tooltip.Trigger>
+                  <Tooltip.Portal>
+                    <Tooltip.Content 
+                      className={styles.tooltip}
+                      sideOffset={5}
+                    >
+                      &lt;3
+                      <Tooltip.Arrow className={styles.tooltipArrow} />
+                    </Tooltip.Content>
+                  </Tooltip.Portal>
+                </Tooltip.Root>
+              </Tooltip.Provider>
+            </div>
+        </footer>
     </nav>
   );
 };
