28653bb0a349aa15338f9afc4e91434eaf97eab1
Aleksandar Bursac <aleksbrsc@gmail.com>
2025-07-05 23:04:45 -0400

    refactor(frontend): moved mock game progress data to /data

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,20 +7,7 @@ import leaderboard from '@assets/images/icons/leaderboard.svg';
 import heart from '@assets/images/icons/heart.svg';
 import { useState, useEffect } from 'react';
 import * as Tooltip from '@radix-ui/react-tooltip';
-
-const gameProgress = {
-    sessionDate: "Sat, Jul 5 | 12:38 AM",
-    rounds: [
-        { round: 1, location: "Jane & Finch", score: 64 },
-        { round: 2, location: "Kensington Marketlol", score: 82 },
-        { round: 3, location: "Bloor West Village", score: 100 },
-        { round: 4, location: "Trinity-Bellwoods", score: 45 },
-        { round: 5, location: "Little Portugal", score: 58 },
-        { round: 6, location: "Regent Park", score: 7 },
-        { round: 7, location: "Roncesvalles", score: 0 },
-        { round: 8, location: "Briar Hill-Belgravia", score: null }
-    ]
-};
+import { gameProgress } from '@data/gameProgress';
 
 const Sidebar = () => {
     const [likeCount, setLikeCount] = useState(() => {
---
+++ frontend/src/data/gameProgress.js
@@ -0,0 +1,13 @@
+export const gameProgress = {
+    sessionDate: "Sat, Jul 5 | 12:38 AM",
+    rounds: [
+        { round: 1, location: "Jane & Finch", score: 64 },
+        { round: 2, location: "Kensington Marketlol", score: 82 },
+        { round: 3, location: "Bloor West Village", score: 100 },
+        { round: 4, location: "Trinity-Bellwoods", score: 45 },
+        { round: 5, location: "Little Portugal", score: 58 },
+        { round: 6, location: "Regent Park", score: 7 },
+        { round: 7, location: "Roncesvalles", score: 0 },
+        { round: 8, location: "Briar Hill-Belgravia", score: null }
+    ]
+};
